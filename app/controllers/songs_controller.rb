17ea0483b4c982063eef0c7de46cab661bf8b8bf
jennyjean <jennyjean8675309@yahoo.com>
2018-11-01 16:41:42 -0400

    Created slug methods and routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,16 @@
+require 'pry'
+
 class SongsController < ApplicationController
 
   get '/songs' do
     @songs = Song.all
 
     erb :'/songs/index'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params["slug"])
+
+    erb :'/songs/show'
   end
 end
