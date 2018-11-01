2c7b67b8071b439dbc92819acbefbdfbc03ab0eb
jennyjean <jennyjean8675309@yahoo.com>
2018-11-01 13:31:52 -0400

    Created genres for our seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+#Creating genres for seed data
+rock = Genre.new(name: "rock")
+pop = Genre.new(name: "pop")
+hip_hop = Genre.new(name: "hip hop")
